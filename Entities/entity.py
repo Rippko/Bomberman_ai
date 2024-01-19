57307f40bdbe25184140ca13673c5eee52a90371
Rippko <adam.mullermilch@gmail.com>
2024-01-19 01:19:33 +0100

    Modified entity with new state mechanism

--- Entities/entity.py
+++ Entities/entity.py
@@ -1,6 +1,8 @@
 import pygame
 from Utilities.sprite_loader import load
 from pygame.math import Vector2
+from States.Entity.idling_state import IdlingState
+from States.Entity.walking_state import WalkingState
 
 class Entity():
     def __init__(self, x: int, y: int, entity_name: str) -> None:
@@ -9,9 +11,13 @@ class Entity():
         self._all_actions = load(entity_name)
         self._direction = Vector2(0, 0)
         self._last_update = pygame.time.get_ticks()
-        self._animation_speed = 100
+        self._animation_speed = 150
         self._current_frame = 0
-        self._current_state = 'Idle'
+        self._current_state = IdlingState('Idle')
+        
+        self.states = {'Idle': IdlingState('Idle'),
+                       'Walking': WalkingState('Walking')
+                       }
         
     def _move_left(self) -> None:
         self._direction.x = -1
@@ -37,16 +43,20 @@ class Entity():
         else:
             return 'front'
         
+    def handle_keypress(self, keys) -> None:
+        self._current_state = self.states[self._current_state.handle_event(keys)]
+        print(self._current_state.get_name())
+        
     def _animate(self, game_display: pygame.display):
         current_time = pygame.time.get_ticks()
         delta_time = current_time - self._last_update
         
-        current_image = self._all_actions[self._current_state][self._get_direction()][self._current_frame]
+        current_image = self._all_actions[self._current_state.get_name()][self._get_direction()][self._current_frame]
         
         game_display.blit(current_image, (self._x, self._y))
         
         if delta_time >= self._animation_speed:
-            if self._current_frame < len(self._all_actions[self._current_state][self._get_direction()]) - 1:
+            if self._current_frame < len(self._all_actions[self._current_state.get_name()][self._get_direction()]) - 1:
                 self._current_frame += 1
             else:
                 self._current_frame = 0
