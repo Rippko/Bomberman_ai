336752eaf97bcfc4334df5733745286e9f1e01fa
Rippko <adam.mullermilch@gmail.com>
2024-01-20 01:28:21 +0100

    Added vertical and horizontal collisions and handlers in player class

--- Entities/entity.py
+++ Entities/entity.py
@@ -3,12 +3,13 @@ from Utilities.sprite_loader import load
 from pygame.math import Vector2
 from States.Entity.idling_state import IdlingState
 from States.Entity.walking_state import WalkingState
+from Utilities.colors import Colors
 
 class Entity():
-    def __init__(self, x: int, y: int, entity_name: str) -> None:
+    def __init__(self, x: int, y: int, entity_name: str, n_frames: int, s_width: int, s_height: int, scale) -> None:
         self._x = x
         self._y = y
-        self._all_actions = load(entity_name)
+        self._all_actions = load(entity_name, n_frames, s_width, s_height, scale)
         self._direction = Vector2(0, 0)
         self._last_update = pygame.time.get_ticks()
         self._animation_speed = 150
@@ -18,6 +19,11 @@ class Entity():
         self.states = {'Idle': IdlingState('Idle'),
                        'Walking': WalkingState('Walking')
                        }
+        
+        self._rect = self._all_actions['Idle']['front'][0].get_rect(center=(x,y))
+        
+        self._movement_speed = 5
+        
         
     def _move_left(self) -> None:
         self._direction.x = -1
@@ -45,7 +51,6 @@ class Entity():
         
     def handle_keypress(self, keys) -> None:
         self._current_state = self.states[self._current_state.handle_event(keys)]
-        print(self._current_state.get_name())
         
     def _animate(self, game_display: pygame.display):
         current_time = pygame.time.get_ticks()
@@ -62,6 +67,48 @@ class Entity():
                 self._current_frame = 0
         
             self._last_update = current_time
+          
+    def _move_horizontal(self):
+        self._x += self._direction.x * self._movement_speed
+        self._rect.x = self._x        
+      
+    def _horizontal_collisions(self, collidables) -> bool:
+        collided = False
         
+        for tile in collidables:
+            if tile.colliderect(self._rect):
+                if self._direction.x < 0:
+                    self._rect.left = tile.right
+                    self._x = self._rect.x
+                    # else:
+                    #     self._move_right()
+                            
+                elif self._direction.x > 0:
+                    self._rect.right = tile.left
+                    self._x = self._rect.x
+                    # else:
+                    #     self._move_left()
+                collided = True
+            
+        return collided
+    
+    def _move_vertical(self):
+        self._y += self._direction.y * self._movement_speed
+        self._rect.y = self._y 
+    
+    def _vertical_collisions(self, collidables) -> bool:
+        collided = False
+        for tile in collidables:
+            if tile.colliderect(self._rect):
+                if self._direction.y > 0:
+                    self._rect.bottom = tile.top
+                    self._y = self._rect.y
+                elif self._direction.y < 0:
+                    self._rect.top = tile.bottom
+                    self._y = self._rect.y
+                collided = True
+        return collided
+    
     def update(self, game_display: pygame.display) -> None:
         self._animate(game_display)
+        
--- Entities/player.py
+++ Entities/player.py
@@ -2,30 +2,38 @@ import pygame
 from pygame.math import Vector2
 from Utilities.sprite_loader import load
 from Entities.entity import Entity
+from Utilities.colors import Colors
 
 class Player(Entity):
-    def __init__(self, x, y, entity_name):
-        super().__init__(x, y, entity_name)
-        self.__movement_speed = 7
+    def __init__(self, x, y, entity_name, n_frames: int, s_width: int, s_height: int, scale) -> None:
+        super().__init__(x, y, entity_name, n_frames, s_width, s_height, scale)
+        self._movement_speed = 7
         
-    def __check_keys(self):
+    def __check_keys(self, pressed_keys) -> None:
         self._direction = Vector2(0, 0)
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_a]:
+        if pressed_keys[pygame.K_a]:
             self._move_left()
-        if keys[pygame.K_d]:
+        elif pressed_keys[pygame.K_d]:
             self._move_right()
-        if keys[pygame.K_w]:
+        elif pressed_keys[pygame.K_w]:
             self._move_up()
-        if keys[pygame.K_s]:
+        elif pressed_keys[pygame.K_s]:
             self._move_down()
+            
+    def __handle_horizontal_collisions(self, collided: bool) -> None:
+        if collided: self._direction.x = 0
+        
+    def __handle_vertical_collisions(self, collided: bool) -> None:
+        if collided: self._direction.y = 0
     
-    def update(self, screen: pygame.display):
-        super().update()
+    def update(self, screen: pygame.display, pressed_keys, collidables: list[pygame.Rect]) -> None:
+        self.__check_keys(pressed_keys)
         
-        self.__check_keys()
-        self._x += self._direction.x * self.__movement_speed
-        self._y += self._direction.y * self.__movement_speed
+        self._move_horizontal()
+        self.__handle_horizontal_collisions(self._horizontal_collisions(collidables))
         
-        screen.blit(self._all_actions['Idle']['front'][0], (self._x, self._y))
+        self._move_vertical()
+        self.__handle_vertical_collisions(self._vertical_collisions(collidables))
         
+        pygame.draw.rect(screen, Colors.GREEN, (self._rect.x, self._rect.y, self._rect.w, self._rect.h))
+        super().update(screen)
