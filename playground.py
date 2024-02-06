b63ffba0c3801aaf6c8a54448c73e5e37e9325b5
Rippko <adam.mullermilch@gmail.com>
2024-02-06 03:17:08 +0100

    Updated playground with new grid generating system

--- playground.py
+++ playground.py
@@ -1,6 +1,7 @@
 import pygame
 from Entities.player import Player
-from Utilities.colors import Colors
+from Utilities.settings import *
+from Obstacles.tile import Tile
 from Obstacles.wall import Wall
 from bomb import Bomb
 import random
@@ -14,40 +15,47 @@ class Playground():
         self.__current_background = self.__origin_background
         self.__current_w = self.__screen.get_width()
         self.__current_h = self.__screen.get_height()
-        self.__player = Player(110, 110, 'player_character', 4, 32, 32, 2)
-        self.__playground = self.__create_playground()
+        
+        self.__origin_grid = []
+        self.__playground = []
+        self.__create_playground()
+        print(len(self.__origin_grid))
+        
+        self.__grid = self.__origin_grid.copy()
+        
+        self.__player = Player(self.__origin_grid[0].x, self.__origin_grid[0].y, 'player_character', 4, 32, 32, 2)
         self.__fullscreen = False
         self.__monitor_resolution = [pygame.display.Info().current_w, pygame.display.Info().current_h]
         
+    def __create_playground(self) -> None:
+        x1, y1 = 0.062 * self.__current_w, 0.11 * self.__current_h
+        x2, y2 = 0.941 * self.__current_w, 0.11 * self.__current_h
+
+        width = x2 - x1
         
+        tile_width = width / 26
         
+        x3, y3 = 0.941 * self.__current_w, 0.11 * self.__current_h
+        x4, y4 = 0.941 * self.__current_w, 0.87 * self.__current_h
+
+        height = y4 - y3
         
-        self.__grid = []
-        # self.__grid.append(pygame.Rect(0.06 * self.__current_w, 0.1 * self.__current_h, 0.88 * self.__current_w, 1))
-        # self.__grid.append(pygame.Rect(0.06 * self.__current_w, 0.1 * self.__current_h, 1, 0.77 * self.__current_h))
-        # self.__grid.append(pygame.Rect(0.06 * self.__current_w, 0.89 * self.__current_h, 0.88 * self.__current_w, 1))
-        # self.__grid.append(pygame.Rect(0.94 * self.__current_w, 0.1 * self.__current_h, 1, 0.77 * self.__current_h))
-        
-    def __create_playground(self) -> list:
-        grid_width = 0.89 * self.__current_w
-        grid_height = 0.80 * self.__current_h
-        cell_size = 65
+        tile_height = height / 13
 
-        playground = []
-
-        rows = int(grid_height / cell_size)
-        cols = int(grid_width / cell_size)
-
-        for row in range(rows):
-            for col in range(cols):
-                x = 0.06 * self.__current_w + col * cell_size
-                y = 0.1 * self.__current_h + row * cell_size
-                create_wall = random.choice([True, False, False])
-
-                if create_wall:
-                    playground.append(Wall(x, y))
-
-        return playground
+        for i in range(26):
+            x = x1 + i * tile_width
+            for j in range(13):
+                y = y1 + j * tile_height
+                self.__origin_grid.append(Tile(x, y))
+                
+        n_walls = len(self.__origin_grid) // 3
+        for i in range(n_walls):
+            index = random.randint(0, len(self.__origin_grid) - 1)
+            if not self.__origin_grid[index].isWall:
+                self.__playground.append(Wall(self.__origin_grid[index].x, self.__origin_grid[index].y, tile_width, tile_height))
+                self.__origin_grid[index].isWall = True
+            else:
+                n_walls += 1
         
     
     def __resize_grid(self, width: int, height: int) -> None:
@@ -55,11 +63,11 @@ class Playground():
         height_ratio = height / self.__current_h
         
         for i in range(len(self.__grid)):
-            self.__grid[i].left = int(self.__grid[i].left * width_ratio)
-            self.__grid[i].top = int(self.__grid[i].top * height_ratio)
+            self.__grid[i].rect.left = int(self.__origin_grid[i].rect.left * width_ratio)
+            self.__grid[i].rect.top = int(self.__origin_grid[i].rect.top * height_ratio)
             
-            self.__grid[i].width = int(self.__grid[i].width * width_ratio)
-            self.__grid[i].height = int(self.__grid[i].height * height_ratio)
+            self.__grid[i].rect.width = int(self.__origin_grid[i].rect.width * width_ratio)
+            self.__grid[i].rect.height = int(self.__origin_grid[i].rect.height * height_ratio)
 
         self.__current_w = width
         self.__current_h = height
@@ -79,9 +87,7 @@ class Playground():
         clock = pygame.time.Clock()
         pygame.init()
         pygame.display.set_caption("Bomberman")
-        collidables = []
-        # collidables = [wall.rect for wall in self.__playground]
-        # collidables.extend(self.__grid)
+        collidables = [wall.rect for wall in self.__playground]
         
         while True:
             for event in pygame.event.get():
@@ -108,21 +114,15 @@ class Playground():
 
             self.__screen.blit(self.__current_background, (0, 0))
             
-            # for tile in collidables:
-            #     pygame.draw.rect(self.__screen, Colors.BLACK, tile)
+            # for tile in self.__grid:
+            #     pygame.draw.rect(self.__screen, BLUE, tile.rect, 2, 2)
             
-            # for wall in self.__playground:
-            #     wall.update(self.__screen)
-                
             for wall in self.__playground:
-                pygame.draw.rect(self.__screen, Colors.RED, wall, 2, 2)
-                
-
-            for rect in self.__grid:
-                pygame.draw.rect(self.__screen, Colors.RED, rect, 5)
-                
+                wall.update(self.__screen)
+                # pygame.draw.rect(self.__screen, RED, wall, 2, 2)
+            
 
             self.__player.update(self.__screen, pressed_keys, collidables)
                         
             pygame.display.flip()
-            clock.tick(60) 
+            clock.tick(60)
