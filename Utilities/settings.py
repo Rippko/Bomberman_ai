b4aa35dfb933f7de758bbc0270e8f75be58b200e
Rippko <adam.mullermilch@gmail.com>
2024-02-06 03:07:43 +0100

    New file replacing colors.py file for constants and setting throughout project

---
+++ Utilities/settings.py
@@ -0,0 +1,11 @@
+# This file contains the settings for the game
+
+# Define some colors
+BLACK = (0, 0, 0)
+GREY = (100, 100, 100)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+TILESIZE = 65
+    
