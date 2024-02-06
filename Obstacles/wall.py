851cc0beed7ab16fc07c73848560756ec2482b0f
Rippko <adam.mullermilch@gmail.com>
2024-02-06 03:15:50 +0100

    Updated input parameters for more precise wall creation

--- Obstacles/wall.py
+++ Obstacles/wall.py
@@ -1,11 +1,12 @@
 import pygame
+from Utilities.settings import *
 
 class Wall():
-    def __init__(self, x: int, y: int) -> None:
-        self.__x = x
-        self.__y = y
-        self.__image = pygame.transform.scale(pygame.image.load('Assets/Wall/wall.png'), (60, 60))
-        self.rect = self.__image.get_rect(topleft = (self.__x, self.__y))
+    def __init__(self, x: int, y: int, width: float, height) -> None:
+        self.x = x
+        self.y = y
+        self.__image = pygame.transform.scale(pygame.image.load('Assets/Wall/wall.png'), (width, height))
+        self.rect = self.__image.get_rect(topleft = (self.x, self.y))
         
     def update(self, game_display: pygame.display):
-        game_display.blit(self.__image, (self.__x, self.__y))
+        game_display.blit(self.__image, (self.x, self.y))
