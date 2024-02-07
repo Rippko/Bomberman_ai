f51375d90eb997143f502312f4bd9c639a3692ef
Adam Fiury <adam.mullermilch@gmail.com>
2024-02-07 10:41:42 +0100

    Updated animation switching for arrow keys

--- States/Entity/idling_state.py
+++ States/Entity/idling_state.py
@@ -6,7 +6,7 @@ class IdlingState(State):
         super().__init__(name)
         
     def handle_event(self, keys) -> str:
-        if keys[pygame.K_a] or keys[pygame.K_d] or keys[pygame.K_w] or keys[pygame.K_s]:
+        if keys[pygame.K_a] or keys[pygame.K_d] or keys[pygame.K_w] or keys[pygame.K_s] or keys[pygame.K_LEFT] or keys[pygame.K_RIGHT] or keys[pygame.K_UP] or keys[pygame.K_DOWN]:
             return 'Walking'
         else:
             return self._name
