14fa197cea2d59e9fab40f73aa0ad102d79b0718
Rippko <adam.mullermilch@gmail.com>
2024-02-08 23:22:11 +0100

    Crate class using singleton

--- Obstacles/crate.py
+++ Obstacles/crate.py
@@ -1,10 +1,12 @@
 import pygame
+from Utilities.settings import *
+from Utilities.asset_loader import AssetLoader
 
 class Crate():
     def __init__(self, x: int, y: int, width: float, height: float) -> None:
         self.x = x
         self.y = y
-        self.image = pygame.transform.scale(pygame.image.load('Assets/Crate/crate.png'), (width, height))
+        self.image = pygame.transform.scale(AssetLoader().crate_img, (width, height))
         self.rect = self.image.get_rect(topleft = (self.x, self.y))
 
     def update(self, game_display):
