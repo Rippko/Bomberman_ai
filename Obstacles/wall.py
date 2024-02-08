6778926a6ce5a2efcd3a2b7e33b3c808f93e66fe
Rippko <adam.mullermilch@gmail.com>
2024-02-08 23:22:46 +0100

    Wall class using singleton

--- Obstacles/wall.py
+++ Obstacles/wall.py
@@ -1,11 +1,12 @@
 import pygame
 from Utilities.settings import *
+from Utilities.asset_loader import AssetLoader
 
 class Wall():
     def __init__(self, x: int, y: int, width: float, height: float) -> None:
         self.x = x
         self.y = y
-        self.image = pygame.transform.scale(pygame.image.load('Assets/Wall/wall.png'), (width, height))
+        self.image = pygame.transform.scale(AssetLoader().wall_img, (width, height))
         self.rect = self.image.get_rect(topleft = (self.x, self.y))
         
     def update(self, game_display: pygame.display):
