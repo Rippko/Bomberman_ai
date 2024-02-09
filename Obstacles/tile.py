c37714868509e6d088775355230a642bbb4cf1dc
Rippko <adam.mullermilch@gmail.com>
2024-02-09 20:25:03 +0100

    Saving tile size into class variable

--- Obstacles/tile.py
+++ Obstacles/tile.py
@@ -5,6 +5,7 @@ class Tile ():
     def __init__(self, x, y, tile_size) -> None:
         self.x = x
         self.y = y
+        self.tile_size = tile_size
         self.isEmpty = False
-        self.rect = pygame.Rect(self.x, self.y, tile_size, tile_size)
+        self.rect = pygame.Rect(self.x, self.y, self.tile_size, self.tile_size)
         
