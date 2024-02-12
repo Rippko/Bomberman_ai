816bd551e16d1b7501572de1071e8656729c5686
Rippko <adam.mullermilch@gmail.com>
2024-02-12 23:24:11 +0100

    Latest version

--- bomb.py
+++ bomb.py
@@ -1,4 +1,6 @@
 import pygame
+from Utilities.settings import *
+from Utilities.sprite_loader import load
 
 class Bomb(pygame.sprite.Sprite):
     def __init__(self, x: int, y: int) -> None:
@@ -14,19 +16,33 @@ class Bomb(pygame.sprite.Sprite):
                 self.images.append(pygame.image.load(f'Assets/Bomb/0{i}.png').convert_alpha())
                 self.images[i] = pygame.transform.scale(self.images[i], (70, 70))
         
+        self.__explosion_images = load('Explosions', 15, 120, 120, 0.5)
+        
         self.index = 0
         self.image = self.images[self.index]
         self.rect = self.image.get_rect(topleft=(self.__x, self.__y))
         self.__explosion_radius = 1
-        self.__bomb_timer = 9
+        self.__bomb_timer = 7
         self.__counter = 0
         
-    def update(self):
+    def __explode(self):
+        pass
+    
+    def draw(self, game_display: pygame.display) -> None:
+        game_display.blit(self.image, (self.__x, self.__y))
+        
+        
+    def update(self, game_display: pygame.display) -> None:        
         self.__counter += 1
         if self.__counter >= self.__bomb_timer and self.index < len(self.images) - 1:
             self.__counter = 0
             self.index += 1
             self.image = self.images[self.index]
+            
+        self.draw(game_display)
         
         if self.index >= len(self.images) - 1 and self.__counter >= self.__bomb_timer:
+            
+            for i in range(1, self.__explosion_radius + 1):
+                self.__explode()
             self.kill()
