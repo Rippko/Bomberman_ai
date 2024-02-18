4fab39e38d1734bc93ab5afee7d9936ba25a020a
Rippko <adam.mullermilch@gmail.com>
2024-02-19 00:04:28 +0100

    Latest player code

--- Entities/player.py
+++ Entities/player.py
@@ -5,18 +5,26 @@ from Obstacles.wall import Wall
 from Obstacles.crate import Crate
 from bomb import Bomb
 from Utilities.settings import *
+from Utilities.observable_object import ObservableObject
 from map import Map
 
-class Player(Entity):
+class Player(Entity, ObservableObject):
     def __init__(self, coords: tuple, entity_name: str, n_frames: int, s_width: int, s_height: int, scale, map: Map, game_display: pygame.display) -> None:
         super().__init__(coords[0], coords[1], entity_name, n_frames, s_width, s_height, scale)
+        ObservableObject.__init__(self)
+        self.add_observer(map)
+        
         self.__game_display = game_display
         self._movement_speed = 5
         self.__map = map
         self.__grid = self.__map.current_map
         self.__bombs = pygame.sprite.Group()
         self.__max_bombs = 1
+        self.current_tile = self.__grid[0][0]
         
+    def notify_observers(self) -> None:
+        for observer in self._observers:
+            observer.update(self)
         
     def __check_keys(self, pressed_keys) -> None:
         self._direction = Vector2(0, 0)
@@ -38,13 +46,20 @@ class Player(Entity):
     def __handle_vertical_collisions(self, collided: bool) -> None:
         if collided: self._direction.y = 0
         
-    def __check_bomb_placement(self, tile) -> bool:
+    def __check_position(self, tile) -> bool:
         return int(self.rect.centerx) in range(int(tile.rect.x), int(tile.rect.x) + int(tile.rect.width)) and int(self.rect.centery) in range(int(tile.rect.y), int(tile.rect.y) + int(tile.rect.height))
 
+    def __determine_position(self) -> None:
+        for row in self.__map.player_map:
+            for tile in row:
+                if self.__check_position(tile):
+                    self.current_tile = tile
+                    self.notify_observers()
+                    
     def __place_bomb(self) -> None:
         for row in self.__grid:
             for tile in row:
-                if self.__check_bomb_placement(tile) and not isinstance(tile, Bomb):
+                if self.__check_position(tile) and not isinstance(tile, Bomb):
                     bomb = Bomb(tile.rect.x, tile.rect.y, self.__game_display)
                     bomb.add_observer(self.__map)
                     self.__bombs.add(bomb)
@@ -53,8 +68,9 @@ class Player(Entity):
     
     def update(self, pressed_keys) -> None:
         self.__check_keys(pressed_keys)
-        # collidables = [tile.rect for row in self.__grid for tile in row if not isinstance(tile, Bomb) and not tile.isEmpty]
         collidables = [tile.rect for row in self.__grid for tile in row if isinstance(tile, Wall) or isinstance(tile, Crate)]
+        
+        #self.__determine_position()
         
         for bomb in self.__bombs:
             if self.rect.colliderect(bomb.rect):
