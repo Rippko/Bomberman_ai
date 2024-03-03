eae813e5c1239c2ed6a81eba612ff612b4c59d02
Rippko <adam.mullermilch@gmail.com>
2024-03-03 17:54:22 +0100

    Added more modularity into sprite_loader

--- Utilities/sprite_loader.py
+++ Utilities/sprite_loader.py
@@ -14,11 +14,12 @@ def get_sprites(file_path: str, n_frames: int, s_width: int, s_height: int, scal
             
         return all_sprites
 
-def load(directory: str, n_frames: int, s_width: int, s_height: int, scale: float) -> dict:
+def load(directory: str, n_frames: tuple, s_width: int, s_height: int, scale: float) -> dict:
     # We need to get the right directory in our file system to load all sprites from which is App/Assets/directory
     asset_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'Assets', directory)
 
     all_actions_dictionary = {}
+    i = 0
     for sub_directory in os.listdir(asset_directory):
         sub_directory_path = os.path.join(asset_directory, sub_directory)
         # Check if sub_directory is a directory and is not empty
@@ -26,8 +27,14 @@ def load(directory: str, n_frames: int, s_width: int, s_height: int, scale: floa
             all_actions_dictionary[sub_directory] = {}
             for image in os.listdir(sub_directory_path):
                 current_image_path = os.path.join(sub_directory_path, image)
-                all_actions_dictionary[sub_directory][image.split('.')[0]] = get_sprites(current_image_path, n_frames, s_width, s_height, scale)
+                all_actions_dictionary[sub_directory][image.split('.')[0]] = get_sprites(current_image_path, n_frames[i], s_width, s_height, scale)
         else:
-            all_actions_dictionary[sub_directory.split('.')[0]] = get_sprites(sub_directory_path, n_frames, s_width, s_height, scale)
+            all_actions_dictionary[sub_directory.split('.')[0]] = get_sprites(sub_directory_path, n_frames[i], s_width, s_height, scale)
+            
+        i += 1
+        
+        
+                        
+        
             
     return all_actions_dictionary
