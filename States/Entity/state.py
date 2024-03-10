87bb7ed6b60581fb2c5357b765ab602569e7daa7
Rippko <adam.mullermilch@gmail.com>
2024-03-10 21:16:52 +0100

    Moved file

---
+++ States/Entity/state.py
@@ -0,0 +1,11 @@
+import pygame
+
+class State():
+    def __init__(self, name) -> None:
+        self._name = name
+        
+    def get_name(self) -> str:
+        return self._name
+    
+    def handle_event(self, keys):
+        pass
