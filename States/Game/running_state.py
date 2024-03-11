ce516d3f6eb17f63ff9a72b3cf2bd8c20fd4276a
Rippko <adam.mullermilch@gmail.com>
2024-03-11 01:09:26 +0100

    Addded second player with different set of controls

--- States/Game/running_state.py
+++ States/Game/running_state.py
@@ -1,4 +1,5 @@
 import pygame
+from Utilities.settings import *
 from .game_state import GameState
 from .pause_state import PauseState
 from Entities.player import Player
@@ -8,7 +9,8 @@ class RunningState(GameState):
     def __init__(self, game) -> None:
         GameState.__init__(self, game)
         self.map = Map(self._game.width, self._game.height)
-        self.player = Player(self.map.set_starting_postion(0, 0), 'player_1', (8, 7, 4), 32, 32, 2.2, self.map, self._game.screen)
+        self.player1 = Player(self.map.set_starting_postion(0, 0), 'player_1', PLAYER_1_CONTROLS, (8, 7, 4), 32, 32, 2.2, self.map, self._game.screen)
+        self.player2 = Player(self.map.set_starting_postion(0, 24), 'player_1', PLAYER_2_CONTROLS, (8, 7, 4), 32, 32, 2.2, self.map, self._game.screen)
         
     def handle_events(self) -> None:
         for event in pygame.event.get():
