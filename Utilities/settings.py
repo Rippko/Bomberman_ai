f2522effda0a779cfe744a649698e7bcd43f7d0b
Rippko <adam.mullermilch@gmail.com>
2024-03-11 01:07:52 +0100

    Added constants for player character controls

--- Utilities/settings.py
+++ Utilities/settings.py
@@ -1,3 +1,4 @@
+import pygame
 # This file contains the settings for the game
 
 # Define some colors
@@ -8,3 +9,19 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
+
+W = pygame.K_w
+S = pygame.K_s
+A = pygame.K_a
+D = pygame.K_d
+SPACE = pygame.K_SPACE
+
+PLAYER_1_CONTROLS = [W, S, A, D, SPACE]
+
+UP = pygame.K_UP
+DOWN = pygame.K_DOWN
+LEFT = pygame.K_LEFT
+RIGHT = pygame.K_RIGHT
+ENTER = pygame.K_RETURN
+
+PLAYER_2_CONTROLS = [UP, DOWN, LEFT, RIGHT, ENTER]
