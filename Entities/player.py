e18932f128e8ab37448a4025a897833d66257593
Rippko <adam.mullermilch@gmail.com>
2024-03-11 01:06:37 +0100

    Modified player class to accept controls as constructor parameters

--- Entities/player.py
+++ Entities/player.py
@@ -9,33 +9,32 @@ from Utilities.observable_object import ObservableObject
 from map import Map
 
 class Player(Entity):
-    def __init__(self, coords: tuple, entity_name: str, n_frames: tuple, s_width: int, s_height: int, scale, map: Map, game_display: pygame.display) -> None:
-        super().__init__(coords[0], coords[1], entity_name, n_frames, s_width, s_height, scale)
-        self.__game_display = game_display
+    def __init__(self, coords: tuple, entity_name: str, key_bindings: list, n_frames: tuple, s_width: int, s_height: int, scale, map: Map, game_display: pygame.display) -> None:
+        super().__init__(coords[0], coords[1], entity_name, key_bindings, n_frames, s_width, s_height, scale)
         self._movement_speed = 5
         self.__map = map
         self.__map.add_player(self)
         self.__grid = self.__map.current_map
+        self.__game_display = game_display
         self.__bombs = pygame.sprite.Group()
         self.__max_bombs = 2
-        self.__bomb_strength = 1
-        
+        self.__bomb_strength = 2
         self.__map_size = self.__map.calculate_game_plan_size()
         
     def __check_keys(self, pressed_keys) -> None:
         self._direction = Vector2(0, 0)
         
         self.handle_keypress(pressed_keys)
-        if (pressed_keys[pygame.K_a] or pressed_keys[pygame.K_LEFT]) and self.rect.x > self.__map_size[0]:
-            self._move_left()
-        elif (pressed_keys[pygame.K_d] or pressed_keys[pygame.K_RIGHT]) and self.rect.x < (self.__map_size[1] - self.rect.width):
-            self._move_right()
-        elif (pressed_keys[pygame.K_w] or pressed_keys[pygame.K_UP]) and self.rect.y > self.__map_size[2]:
+        if (pressed_keys[self._controls[0]]) and self.rect.y > self.__map_size[2]:
             self._move_up()
-        elif (pressed_keys[pygame.K_s] or pressed_keys[pygame.K_DOWN]) and self.rect.y < (self.__map_size[3] - self.rect.height - 5):
+        elif (pressed_keys[self._controls[1]]) and self.rect.y < (self.__map_size[3] - self.rect.height - 5):
             self._move_down()
+        elif (pressed_keys[self._controls[2]]) and self.rect.x > self.__map_size[0]:
+            self._move_left()
+        elif (pressed_keys[self._controls[3]]) and self.rect.x < (self.__map_size[1] - self.rect.width):
+            self._move_right()
                 
-        if pressed_keys[pygame.K_SPACE] and len(self.__bombs) < self.__max_bombs:
+        if pressed_keys[self._controls[4]] and len(self.__bombs) < self.__max_bombs:
             self.__place_bomb()
             
     def __handle_horizontal_collisions(self, collided: bool) -> None:
