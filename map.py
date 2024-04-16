448b05159cd3f15cc14f509f040a5b3c4f4c2536
Rippko <adam.mullermilch@gmail.com>
2024-04-17 01:12:15 +0200

    Removed unnecessary print statement

--- map.py
+++ map.py
@@ -155,7 +155,6 @@ class Map(Observer):
         #         self.origin_map[y][x] = Crate(self.origin_map[y][x].x, self.origin_map[y][x].y, tile_width, tile_height)
         
     def set_starting_postion(self, x: int, y: int) -> tuple:
-        print(self.origin_map[x][y].x, self.origin_map[x][y].y)
         return (self.origin_map[x][y].x, self.origin_map[x][y].y)
     
     def add_player(self, player) -> None:
