6ca82528f89c640dcbee1844ea6757766bf1643b
Adam Fiury <adam.mullermilch@gmail.com>
2024-04-22 22:52:37 +0200

    Refactored button and add draw text method for writing any text inside button

--- Utilities/button.py
+++ Utilities/button.py
@@ -1,13 +1,23 @@
 import pygame
+from Utilities.settings import *
 
 class Button():
-    def __init__(self, x, y, image: pygame.image, scale):
+    def __init__(self, x, y, image: pygame.image, text: str, scale):
         width = image.get_width()
         height = image.get_height()
+        self.x = x
+        self.y = y
         self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
         self.clicked = False
+        self.text = text
+        self.font = pygame.font.Font('Assets/Fonts/VCR_OSD_MONO_1.001.ttf', 40)
+
+    def draw_text(self, surface) -> None:
+        text_surface = self.font.render(self.text, True, WHITE)
+        text_rect = text_surface.get_rect(center=(self.x, self.y))
+        surface.blit(text_surface, (text_rect.x, text_rect.y))
 
     def check_clicked(self) -> bool:
         action = False
@@ -25,3 +35,4 @@ class Button():
 
     def draw(self, surface):
         surface.blit(self.image, (self.rect.x, self.rect.y))
+        self.draw_text(surface)
