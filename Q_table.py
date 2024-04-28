b5bbfe35961aacbf34be3dc75fd3ffb40c475c59
Rippko <adam.mullermilch@gmail.com>
2024-04-28 23:32:55 +0200

    Added Q_table as class

---
+++ Q_table.py
@@ -0,0 +1,24 @@
+import pickle
+
+class Q_table:
+    def __init__(self, filename):
+        self.filename = filename
+        self.data = {}
+    
+    def save_Q_table(self):
+        with open(self.filename, 'wb') as f:
+            pickle.dump(self.Q_table, f)
+    
+    def load_Q_table(self) -> None:
+        try:
+            with open(self.filename,'rb') as file:
+                    self.Q_table = pickle.load(file)
+            return True
+        except FileNotFoundError:
+            print(f"Error: File not found - {self.filename}")
+            return False
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return False
+        
+    
